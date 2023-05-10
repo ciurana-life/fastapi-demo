df4a035c9fb2e3350bbb3ffd3355ea06fe05c0ee
Victor Ciurana <asuka@Victors-MacBook-Pro.local>
2023-05-10 14:47:25 +0200

    remove print statements

--- app/db/session.py
+++ app/db/session.py
@@ -11,9 +11,7 @@ database = env.str("DATABASE_URL", default="sqlite:///./sql_app.db")
 if "sqlite" in database:  # pragma: no cover
     engine = create_engine(database, connect_args={"check_same_thread": False})
     Base.metadata.create_all(bind=engine)
-    print("USING SQLITE")
 else:
     engine = create_engine(database, echo=True)
-    print("USING POSTGRESS")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
